c41f064c03f3a988d169b91a29607c7e1c398a01
lideming <me@yuuza.net>
2020-11-01 05:15:02 +0800

    Allow no configuration file

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,22 @@
 #!/usr/bin/node
 import { cwd } from "process";
+import { stat } from "fs/promises";
 import { build, Config } from "./builder";
 
-import(cwd() + '/buildconfig.js').then(c => {
-    const config = c.default as Config;
+const configPath = cwd() + '/buildconfig.js';
+
+stat(configPath).then((s) => {
+    return s.isFile();
+}, (e) => {
+    if (e.code === 'ENOENT') {
+        return false;
+    }
+}).then(s => {
+    if (s) {
+        return import(configPath).then(x => x.default)
+    }
+    return {};
+}).then(c => {
+    const config = c as Config;
     return build(config);
-});
+})
